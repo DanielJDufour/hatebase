0136fbbb06c45bb65eb372042360622d6f28f23f
DanielJDufour <daniel.j.dufour@gmail.com>
2017-06-13 03:58:06 +0000

    added setup files

---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+  name = 'hatebase',
+  packages = ['hatebase'],
+  package_dir = {'hatebase': 'hatebase'},
+  package_data = {'hatebase': ['__init__.py', 'tests/__init__.py', 'tests/test.py']},
+  version = '0.1',
+  description = "Python Version of Andrew Welter's Hatebase Wrapper",
+  author = 'Daniel J. Dufour',
+  author_email = 'daniel.j.dufour@gmail.com',
+  url = 'https://github.com/DanielJDufour/hatebase',
+  download_url = 'https://github.com/DanielJDufour/hatebase/tarball/download',
+  keywords = ['conflict', 'language', 'nlp', 'python', 'tagging'],
+  classifiers = [],
+  install_requires=["requests"]
+)

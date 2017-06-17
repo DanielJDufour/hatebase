11286591375d66e71e5faa0163ce13d13c8cd8ad
DanielJDufour <daniel.j.dufour@gmail.com>
2017-06-17 15:48:06 +0000

    Updated Readme for PyPi

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
   packages = ['hatebase'],
   package_dir = {'hatebase': 'hatebase'},
   package_data = {'hatebase': ['__init__.py', 'tests/__init__.py', 'tests/test.py']},
-  version = '0.3',
+  version = '0.4',
   description = "Python Version of Andrew Welter's Hatebase Wrapper",
   author = 'Daniel J. Dufour',
   author_email = 'daniel.j.dufour@gmail.com',
